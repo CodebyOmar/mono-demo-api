8c7f675622a143ee3f2e299c7d4f40c7d1688e3f
codebyomar <omarmohammed580@gmail.com>
2020-08-08 03:35:52 +0100

    allow wildcard port number

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,6 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(4006, "0.0.0.0");
+  await app.listen(process.env.PORT || 4006, "0.0.0.0");
 }
 bootstrap();
